c33ac0c929bfc644ed325eebacd1f499877d50f6
ezQel <ezekiel3499@gmail.com>
2016-01-07 16:07:08 +0300

    evaluating data as functions

--- modules/eval.js
+++ modules/eval.js
@@ -4,5 +4,7 @@ function evalAndReturnX(code) {
     eval(code);
     return x;
 }
-
 console.log(evalAndReturnX('x = x + 2'));
+
+let sum = Function('a, b', 'return a + b');
+console.log(sum(3, 4));
