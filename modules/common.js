84518b6ad30e2811f06f2d26c2d318882fec2b7f
ezQel <ezekiel3499@gmail.com>
2016-01-07 17:25:55 +0300

    commonJS

---
+++ modules/common.js
@@ -0,0 +1,3 @@
+const {operate} = require('./operate.js');
+
+console.log(operate(50, 50));
--- modules/operate.js
+++ modules/operate.js
@@ -1,9 +1,9 @@
 // this module uses other modules to return an object of the division, multiplication and sum of two numbers.
 
-import sum from sum;
-import divide from divide;
-import multiply from multiply;
+const {sum} = require("./sum");
+const {divide} = require("./divide");
+const {multiply} = require("./multiply");
 
 exports.operate = (a, b) => {
-    return {'sum' : sum(a, b), 'division' : divide(a, b), 'multiply' : multiply(a,b)};
+    return {'sum' : sum(a, b), 'division' : divide(a, b), 'multiplication' : multiply(a,b)};
 }
