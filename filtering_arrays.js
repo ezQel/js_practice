57ee13f60d53b2c92810a409ac17e4c90e1ceff0
ezQel <ezekiel3499@gmail.com>
2019-12-23 09:32:18 +0300

    filtering arrays

---
+++ filtering_arrays.js
@@ -0,0 +1,6 @@
+let arr = [ 'human', 'house', 'car', 'tree', 'dog', 'cat', 'cellphone'];
+
+let h = arr.filter(el => el.startsWith('h'));
+let c = arr.filter(el => el.startsWith('c'));
+console.log(h)
+console.log(c)
