50f4aaf06e6dd62ffd8e9d4881c16cba9e69cab2
ezQel <ezekiel3499@gmail.com>
2020-01-01 12:05:06 +0300

    testing for matches

--- Regular Expressions/regExp.js
+++ Regular Expressions/regExp.js
@@ -4,3 +4,7 @@ let re2 = /abc/;
 
 // some characters such as ? and + have special meanings and must be preceeded with a backslash. e.g
 let eighteenplus = /eighteen\+/;
+
+// testing for matches
+console.log(re.test('abcsdddf')); //test returns true if the regex pattern is found in the string
+console.log(re2.test('abdkdg')); //and false if the pattern is not found;
