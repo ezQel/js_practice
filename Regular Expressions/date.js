0beb2c849d7ad0c93b49867fb5b896b247818648
ezQel <ezekiel3499@gmail.com>
2020-01-06 23:46:17 +0300

    date class

---
+++ Regular Expressions/date.js
@@ -0,0 +1,9 @@
+let now = new Date();
+console.log(now);
+console.log(now.getTime()); // seconds elapsed since 1970
+
+let past = new Date(2008, 05, 30, 20, 30, 46, 999);
+console.log(past);
+console.log(past.getTime());
+
+console.log(new Date(944963853988));
