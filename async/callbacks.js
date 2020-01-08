ad3cfbb320543ad4849b730d1d74fb79aade3c54
ezQel <ezekiel3499@gmail.com>
2020-01-08 12:08:33 +0300

    setTimeout

---
+++ async/callbacks.js
@@ -0,0 +1,2 @@
+// One approach to asynchronous programming is to make functions that perform a slow action take an extra argument,
+// a callback function.
---
+++ async/timeout.js
@@ -0,0 +1,3 @@
+// setTimeout function waits a given number of milliseconds and then calls a function.
+
+setTimeout(() => console.log('wait time elapsed'), 2000);
