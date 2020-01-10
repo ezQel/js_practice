249049bf19a76595ccad569dfcdeaf414f9eed04
ezQel <ezekiel3499@gmail.com>
2020-01-10 11:13:56 +0300

    finding elements by id

--- DOM/document.js
+++ DOM/document.js
@@ -1,9 +1,9 @@
 // get elements by tag name
 
-// let firstLink = document.body.getElementsByTagName('a')[0];
-// console.log(firstLink.href);
+let firstLink = document.body.getElementsByTagName('a')[0];
+console.log(firstLink.href);
 
 // get specific elements by id
 
-let w3c = document.body;
-console.log(w3c);
+let w3c = document.getElementById('W3c');
+console.log(w3c.href);
