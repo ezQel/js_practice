fd8e0edc25122c591e74895dd367996d1ea7fc81
ezQel <ezekiel3499@gmail.com>
2020-01-10 11:06:08 +0300

    moved script tag in html doc to the bottom

--- DOM/document.js
+++ DOM/document.js
@@ -1,6 +1,9 @@
 // get elements by tag name
 
-let firstLink = document.body.getElementsByTagName('a')[0];
-console.log(firstLink.href);
+// let firstLink = document.body.getElementsByTagName('a')[0];
+// console.log(firstLink.href);
 
 // get specific elements by id
+
+let w3c = document.body;
+console.log(w3c);
