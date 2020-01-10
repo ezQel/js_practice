358a78ece866e4163d0340c16248e827ab0c5e08
ezQel <ezekiel3499@gmail.com>
2020-01-10 10:45:36 +0300

    added id to w3c link

--- DOM/document.js
+++ DOM/document.js
@@ -1,9 +1,6 @@
 // get elements by tag name
-try{
-    let firstLink = document.body.getElementsByTagName('a')[0];
-    console.log(firstLink.href);
-}
-catch(error){
-    console.log(error);
 
-}
+let firstLink = document.body.getElementsByTagName('a')[0];
+console.log(firstLink.href);
+
+// get specific elements by id
