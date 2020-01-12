1da214da4f7fb226bf049de4323705891f5383b5
ezQel <ezekiel3499@gmail.com>
2020-01-12 11:48:10 +0300

    replacing nodes

--- DOM/document.js
+++ DOM/document.js
@@ -8,9 +8,21 @@ console.log(firstLink.href);
 let w3c = document.getElementById('W3c');
 console.log(w3c.href);
 
-let paragraphs = document.body.getElementsByTagName('p'); //gets a NodeArray object of all paragraphs in the document.
+let paragraphs = document.body.getElementsByTagName('p'); //gets a Node list object of all paragraphs in the document.
 setTimeout(() => {
     document.body.insertBefore(paragraphs[2], paragraphs[0]); // removes paragraph three and puts it to the top
     setTimeout(() => document.body.insertBefore(paragraphs[2],paragraphs[1]), 2000); // removes new paragraph three and puts it to second
-
 }, 2000); 
+
+function replaceImages() {
+    let images = document.body.getElementsByTagName("img");
+    for (let i = images.length - 1; i >= 0; i--) {
+    let image = images[i];
+    if (image.alt) {
+    let text = document.createTextNode(image.alt);
+    image.parentNode.replaceChild(text, image);
+    }
+    }
+    }
+
+    
