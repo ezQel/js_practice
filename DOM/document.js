7a6a7442d2687191c815979ab14c124ed7a1c02e
ezQel <ezekiel3499@gmail.com>
2020-01-12 11:18:02 +0300

    changing the document

--- DOM/document.js
+++ DOM/document.js
@@ -7,3 +7,10 @@ console.log(firstLink.href);
 
 let w3c = document.getElementById('W3c');
 console.log(w3c.href);
+
+let paragraphs = document.body.getElementsByTagName('p'); //gets a NodeArray object of all paragraphs in the document.
+setTimeout(() => {
+    document.body.insertBefore(paragraphs[2], paragraphs[0]); // removes paragraph three and puts it to the top
+    setTimeout(() => document.body.insertBefore(paragraphs[2],paragraphs[1]), 2000); // removes new paragraph three and puts it to second
+
+}, 2000); 
