36a1c9cc03033adeb882ff3cefd408e7c87d03c2
ezQel <ezekiel3499@gmail.com>
2020-01-12 12:14:52 +0300

    positioning and animating

--- DOM/document.js
+++ DOM/document.js
@@ -26,3 +26,15 @@ function replaceImages() {
     }
 
 w3c.style.color = 'green'; // change w3c link color to green
+
+let cat = document.querySelector('p img');
+let angle = Math.PI / 2;
+function animate(time, lastTime) {
+if (lastTime != null) {
+angle += (time - lastTime) * 0.001;
+}
+cat.style.top = (Math.sin(angle) * 20) + "px";
+cat.style.left = (Math.cos(angle) * 200) + "px";
+requestAnimationFrame(newTime => animate(newTime, time));
+}
+requestAnimationFrame(animate);
