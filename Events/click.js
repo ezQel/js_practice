4f4fb55fb64bb86cfc5cc78ddbad35f68bfd257e
ezQel <ezekiel3499@gmail.com>
2020-01-13 22:41:35 +0300

    event objects

--- Events/click.js
+++ Events/click.js
@@ -1,4 +1,14 @@
-window.addEventListener('click', () => console.log('You clicked'));
+window.addEventListener('mousedown', (event) => {
+   if (event.button == 0){
+       console.log('Left Button Clicked');
+   }
+   else if (event.button == 1){
+       console.log('Middle Button Clicked');
+   }
+   else if (event.button == 2){
+       console.log('Right Button Clicked');
+   }
+});
 
 let button = document.querySelector('button');
 function once(params) {
