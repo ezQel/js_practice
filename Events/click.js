a2be53e5907fcf0f31b5a6aac0132afbe4b68d78
ezQel <ezekiel3499@gmail.com>
2020-01-13 22:33:16 +0300

    removing event listener

--- Events/click.js
+++ Events/click.js
@@ -1,4 +1,10 @@
 window.addEventListener('click', () => console.log('You clicked'));
 
 let button = document.querySelector('button');
-button.addEventListener('click', () => console.log('You clicked the button'));
+function once(params) {
+    console.log('one-off event done');
+    button.removeEventListener('click', once); // removeEventListener requires exact same arguments as those given to addEventListener
+        
+}
+button.addEventListener('click', once);
+
