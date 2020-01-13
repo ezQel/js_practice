974b8a03ef7405a2babe6d6e2c17bf4058b5ed78
ezQel <ezekiel3499@gmail.com>
2020-01-13 22:28:38 +0300

    handling click event for a specific node

--- Events/click.js
+++ Events/click.js
@@ -1,1 +1,4 @@
 window.addEventListener('click', () => console.log('You clicked'));
+
+let button = document.querySelector('button');
+button.addEventListener('click', () => console.log('You clicked the button'));
