47e3ac1e69099376f158ff0089c2309b1c0b2992
ezQel <ezekiel3499@gmail.com>
2020-01-14 21:13:40 +0300

    stopPropagation

--- Events/propagation.js
+++ Events/propagation.js
@@ -1,10 +1,7 @@
 let bp = document.getElementById('btn');
 bp.addEventListener('mousedown', handlebp);
 function handlebp(event) {
-    if (event.button == 0) {
-        event.stopPropagation();
-    }
-    else if (event.button == 2){
+   if (event.button == 2){
         console.log('right button clicked on BUTTON');
         event.stopPropagation();
     }
