ae8ddeea07d03cad0f2487c5c741724016a35854
ezQel <ezekiel3499@gmail.com>
2020-01-14 21:24:09 +0300

    event target

--- Events/propagation.js
+++ Events/propagation.js
@@ -8,3 +8,12 @@ function handlebp(event) {
 }
 
 window.addEventListener('mousedown', () => console.log('button clicked'));
+
+//Most event objects have a target property that refers to the node where they originated
+let div = document.getElementById('target');
+div.addEventListener('mousedown', (event) => {
+    event.stopPropagation();
+    if (event.target.nodeName == 'BUTTON'){
+        console.log('button', event.target.textContent,'Clicked');
+    }
+});
