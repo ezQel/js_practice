633956a8e4b453ac6ff1a581b08dfc9162b3fd9f
ezQel <ezekiel3499@gmail.com>
2020-01-15 11:50:24 +0300

    focus and blur events

--- Events/scroll.js
+++ Events/scroll.js
@@ -7,3 +7,15 @@ function updateProgress(event) {
     let max = document.body.scrollHeight - innerHeight;
     progressBar.style.width = `${pageYOffset / max * 100}%`;
 }
+
+let help = document.querySelector("#help");
+let fields = document.querySelectorAll("input");
+for (let field of Array.from(fields)) {
+field.addEventListener("focus", event => {
+let text = event.target.getAttribute("data-help");
+help.textContent = text;
+});
+field.addEventListener("blur", event => {
+help.textContent = "";
+});
+}
