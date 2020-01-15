1d0016da7d32cef19a87070267de5295c26d9b54
ezQel <ezekiel3499@gmail.com>
2020-01-15 11:35:25 +0300

    scroll events

--- Events/scroll.js
+++ Events/scroll.js
@@ -3,3 +3,7 @@ let progressBar = document.getElementById('progress');
 
 window.addEventListener('scroll', updateProgress);
 
+function updateProgress(event) {
+    let max = document.body.scrollHeight - innerHeight;
+    progressBar.style.width = `${pageYOffset / max * 100}%`;
+}
